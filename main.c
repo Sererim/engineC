c0c4cd2b0d0d274502eeebfa54b93e432d8295ef
Sererim <danielleymanw@gmail.com>
2024-09-15 16:47:07 +0300

    IT IS SHOOTING STUFF

--- main.c
+++ main.c
@@ -1,23 +1,84 @@
 
 int entry(int argc, char **argv) {
+
+
+	window.title  = STR("Le Game");
+	window.width  = 960;
+	window.height = 720;
+
+	window.clear_color = hex_to_rgba(0x02a2d3aff);
 	
-	// This is how we (optionally) configure the window.
-	// To see all the settable window properties, ctrl+f "struct Os_Window" in os_interface.c
-	window.title = STR("GAME AHAHAHAHAAH");
-	
+	Gfx_Image *player = load_image_from_disk(fixed_string("player.png"), get_heap_allocator());
+	assert(player, "ERROR: player is not found!");
+
+	// Player movement.
+	Vector2 player_input    = v2(0, 0);
+	Vector2 player_position = v2(0, 0);
+
+	// Time keeping.
+	float64 last_time   = os_get_elapsed_seconds();
+	float64 now         = 0.0f;
+	float64 delta_t     = 0.0f;
+	float64 sec_counter = 0.0f;
+
+	int frame_count = 0;
+
+	// Bullet ?
+	Vector2 bullet_position  = v2(.0f, .0f);
+	bool shoot_bullet        = false;
+
 	while (!window.should_close) {
 		reset_temporary_storage();
+
+		now = os_get_elapsed_seconds();
+		delta_t = now - last_time;
 		
-		float64 now = os_get_elapsed_seconds();
+		os_update();
+
+		if (is_key_just_pressed(KEY_ESCAPE)) {
+			window.should_close = true;
+		}
 		
-		Matrix4 rect_xform = m4_scalar(1.0);
-		rect_xform         = m4_rotate_z(rect_xform, (f32)now);
-		rect_xform         = m4_translate(rect_xform, v3(-125, -125, 0));
-		draw_rect_xform(rect_xform, v2(250, 250), COLOR_GREEN);
-		
-		draw_rect(v2(sin(now)*window.width*0.4-60, -60), v2(120, 120), COLOR_RED);
-		
-		os_update(); 
+		player_input = v2(0, 0);
+		if (is_key_down('A')) {
+			player_input.x -= 1.0f;
+		}
+		if (is_key_down('D')) {
+			player_input.x += 1.0f;
+		}
+		if (is_key_down('S')) {
+			player_input.y -= 1.0f;
+		}
+		if (is_key_down('W')) {
+			player_input.y += 1.0f;
+		}
+		if (is_key_down(KEY_SPACEBAR)) {
+			shoot_bullet = true;
+			bullet_position.x = player_position.x;
+			bullet_position.y = player_position.y;
+		}
+
+		player_input = v2_normalize(player_input);
+
+		player_position = v2_add(player_position, 
+		v2_mulf(player_input, 0.08f * delta_t));
+
+		Matrix4 xform = m4_scalar(1.0);
+		xform         = m4_translate(xform, v3(
+			player_position.x, player_position.y, 0));
+
+		draw_image_xform(player, xform, v2(250, 250), COLOR_BLUE);
+
+		if (shoot_bullet) {
+			Matrix4 bullet_form = m4_scalar(1.0f);
+			bullet_form         = m4_translate(
+				bullet_form, v3(bullet_position.x + 113, bullet_position.y + 125, 0));
+
+			draw_rect_xform(bullet_form, v2(15, 50), COLOR_GREEN);
+
+			bullet_position.y += (f32) (0.08f * delta_t);
+		}
+		 
 		gfx_update();
 	}
 
